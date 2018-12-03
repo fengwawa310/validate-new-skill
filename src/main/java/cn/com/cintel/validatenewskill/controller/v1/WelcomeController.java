949379f1513a4a4441f2436c7f6c6476c57627f6
wangchao <chao.wang@cintel.com.cn>
2018-12-03 10:44:11 +0800

    代码提交git分支

---
+++ src/main/java/cn/com/cintel/validatenewskill/controller/v1/WelcomeController.java
@@ -0,0 +1,23 @@
+package cn.com.cintel.validatenewskill.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @company Name:
+ * @author: sky
+ * @creatDate: 2018/12/3 10:41 AM
+ * @className: WelcomeController
+ * @description: 测试启动接口
+ * @modified By:
+ * @modifyDate:
+ */
+@RestController
+public class WelcomeController {
+
+    @GetMapping(value = "/welcome")
+    public void welcome(){
+        System.out.println("Hello World!");
+    }
+
+}
